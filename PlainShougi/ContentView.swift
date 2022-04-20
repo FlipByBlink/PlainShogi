7a2327b3368f7f4e8f46d3fbb93c2f08b03cc4bc
aaaa <aaaa>
2022-04-20 15:49:36 +0900

    Initial Commit

---
+++ PlainShougi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlainShougi
+//
+//  Created by nnname on 2022/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlainShougi/PlainShougiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlainShougiApp.swift
+//  PlainShougi
+//
+//  Created by nnname on 2022/04/20.
+//
+
+import SwiftUI
+
+@main
+struct PlainShougiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
