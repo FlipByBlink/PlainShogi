565c270bdc4d8e6535cf44e0fc423fb8262d7c5b
aaaa <aaaa>
2022-06-28 12:18:43 +0900

    a

--- PlainShogi/ContentView.swift
+++ PlainShogi/ContentView.swift
@@ -59,16 +59,18 @@ struct マス: View {
     
     var body: some View {
         if let 駒 = 📱.駒の配置[位置] {
-            コマ(駒.職名, 余白なし: true)
-                .rotationEffect(下向き(駒.陣営 == .玉側))
+//            コマ(駒.職名, 余白なし: true)
+            盤上のコマ(駒.陣営, 駒.職名)
+//                .rotationEffect(下向き(駒.陣営 == .玉側))
                 .onDrag {
                     📱.盤上の駒を持ち上げる(位置)
                 } preview: {
-                    コマ(駒.職名)
-                        .environmentObject(📱)
-                        .border(.primary)
-                        .rotationEffect(下向き(駒.陣営 == .玉側))
-                        .onAppear { 振動フィードバック() }
+                    コマのプレビュー(駒.陣営, 📱.この駒の表記(駒.職名))
+//                    コマ(駒.職名)
+//                        .environmentObject(📱)
+//                        .border(.primary)
+//                        .rotationEffect(下向き(駒.陣営 == .玉側))
+//                        .onAppear { 振動フィードバック() }
                 }
                 .onDrop(of: [.utf8PlainText], isTargeted: nil) { 📦 in
                     📱.駒をここに置く(位置, 📦)
@@ -77,7 +79,8 @@ struct マス: View {
                     📱.駒を裏返す(位置)
                 }
         } else {
-            Color(uiColor: .systemBackground)
+            Rectangle()
+                .foregroundStyle(.background)
                 .onDrop(of: [.utf8PlainText], isTargeted: nil) { 📦 in
                     📱.駒をここに置く(位置, 📦)
                 }
@@ -86,69 +89,69 @@ struct マス: View {
 }
 
 
-struct コマ: View {
-    @EnvironmentObject var 📱: 📱AppModel
-    
-    var 職名: 駒の種類
-    
-    var 余白なし: Bool
-    
-    var 手駒の数: Int
-    
-    var 表記: String {
-        let 一文字 = 📱.🚩English表記 ? 職名.English表記 : 職名.rawValue
-        
-        if 手駒の数 > 1 {
-            return 一文字 + 手駒の数.description
-        } else {
-            return 一文字
-        }
-    }
-    
-    var body: some View {
-        ZStack {
-            Color(uiColor: .systemBackground)
-            
-            if 余白なし {
-                Text(表記)
-            } else {
-                Text(表記)
-                    .padding()
-            }
-        }
-        .minimumScaleFactor(0.1)
-        .accessibilityHidden(true)
-    }
-    
-    init(_ ｼｮｸﾒｲ:駒の種類, _ ｶｽﾞ:Int = 1, 余白なし ﾖﾊｸﾅｼ:Bool = false) {
-        職名 = ｼｮｸﾒｲ
-        手駒の数 = ｶｽﾞ
-        余白なし = ﾖﾊｸﾅｼ
-    }
-}
+//struct コマ: View {
+//    @EnvironmentObject var 📱: 📱AppModel
+//    
+//    var 職名: 駒の種類
+//    
+//    var 余白なし: Bool
+//    
+//    var 手駒の数: Int
+//    
+//    var 表記: String {
+//        let 一文字 = 📱.🚩English表記 ? 職名.English表記 : 職名.rawValue
+//        
+//        if 手駒の数 > 1 {
+//            return 一文字 + 手駒の数.description
+//        } else {
+//            return 一文字
+//        }
+//    }
+//    
+//    var body: some View {
+//        ZStack {
+//            Color(uiColor: .systemBackground)
+//            
+//            if 余白なし {
+//                Text(表記)
+//            } else {
+//                Text(表記)
+//                    .padding()
+//            }
+//        }
+//        .minimumScaleFactor(0.1)
+//        .accessibilityHidden(true)
+//    }
+//    
+//    init(_ ｼｮｸﾒｲ:駒の種類, _ ｶｽﾞ:Int = 1, 余白なし ﾖﾊｸﾅｼ:Bool = false) {
+//        職名 = ｼｮｸﾒｲ
+//        手駒の数 = ｶｽﾞ
+//        余白なし = ﾖﾊｸﾅｼ
+//    }
+//}
 
 
 struct 盤上のコマ: View {
     @EnvironmentObject var 📱: 📱AppModel
     
-    var 職名: 駒の種類
+    var 陣営: 王側か玉側か
     
-    var 表記: String {
-        📱.🚩English表記 ? 職名.English表記 : 職名.rawValue
-    }
+    var 職名: 駒の種類
     
     var body: some View {
         ZStack {
             Rectangle()
                 .foregroundStyle(.background)
             
-            Text(表記)
+            Text(📱.この駒の表記(職名))
                 .minimumScaleFactor(0.1)
+                .rotationEffect(下向き(陣営 == .玉側))
                 .accessibilityHidden(true)
         }
     }
     
-    init(_ ｼｮｸﾒｲ:駒の種類) {
+    init(_ ｼﾞﾝｴｲ: 王側か玉側か, _ ｼｮｸﾒｲ: 駒の種類) {
+        陣営 = ｼﾞﾝｴｲ
         職名 = ｼｮｸﾒｲ
     }
 }
@@ -202,17 +205,19 @@ struct 盤外のコマ: View {
     
     var 職名: 駒の種類
     
+    var 駒の表記: String {
+        📱.この駒の表記(職名)
+    }
+    
     var 手駒の数: Int {
         📱.この手駒の数(陣営, 職名)
     }
     
-    var 表記: String {
-        let 🪧 = 📱.🚩English表記 ? 職名.English表記 : 職名.rawValue
-        
+    var 手駒の数の表記: String {
         if 手駒の数 >= 2 {
-            return 🪧 + 手駒の数.description
+            return 手駒の数.description
         } else {
-            return 🪧
+            return ""
         }
     }
     
@@ -224,13 +229,13 @@ struct 盤外のコマ: View {
                 Rectangle()
                     .foregroundStyle(.background)
                 
-                Text(表記)
+                Text(駒の表記 + 手駒の数の表記)
                     .minimumScaleFactor(0.1)
                     .accessibilityHidden(true)
                     .onDrag{
-                        📱.手駒を持ち上げる(将棋駒(陣営,職名))
+                        📱.手駒を持ち上げる(将棋駒(陣営, 職名))
                     } preview: {
-                        コマのプレビュー(陣営, 表記)
+                        コマのプレビュー(陣営, 駒の表記)
 //                        コマ(職名)
 //                            .environmentObject(📱)
 //                            .border(.primary)
@@ -275,6 +280,7 @@ struct コマのプレビュー: View { //TODO: 実装する？
         表記 = ﾋｮｳｷ
     }
 }
+
 
 func 下向き(_ 玉側かどうか: Bool) -> Angle {
     if 玉側かどうか {
