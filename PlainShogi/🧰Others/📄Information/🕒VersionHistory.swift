c5a4a9c5d9f29ba5f8ec2fd74567139230f30f26
aaaa <aaaa>
2022-07-08 16:35:59 +0900

    a

--- PlainShogi/🧰Others/📄Information/🕒VersionHistory.swift
+++ PlainShogi/🧰Others/📄Information/🕒VersionHistory.swift
@@ -32,8 +32,8 @@ let 🕒LatestVersionDescription = """
 - 盤面の駒を盤外へ直接移動できるように変更
 - 簡単に駒を消したり増やしたりする機能の追加
 - 初期化等のショートカットをメニューボタン長押しで呼び出す機能を追加
-- アプリ内課金で広告非表示にできる機能を実装
 - 手駒を3回タップして消す機能の削除
+- アプリ内課金で広告非表示にできる機能を実装
 - アプリ内広告バナーの表示頻度を「3回起動毎に起動直後1回」から「起動6回以降は起動直後毎回」に変更
 - 広くリファクタリング
 - いくつかの小さめの改善やバグ修正
@@ -41,8 +41,8 @@ let 🕒LatestVersionDescription = """
 - Directly move a piece to out-of-board
 - Add Edit Mode
 - Add shortcut function by long press menu button
-- Add In-App Purchase to hide AD
 - Delete function to delete a piece by 3 tapping
+- Add In-App Purchase to hide AD
 - Change frequency in AD ("1 in 3" → "after 5, every")
 - Widely refactoring
 - Various bugfixes and improvements
--- README.md
+++ README.md
@@ -21,16 +21,16 @@ iOSの標準デザインに沿ったシンプルな将棋盤アプリです。
 
 文字のサイズや太さなどはOSシステム設定に適応して変わります。
 
-盤面をテキストデータとして書き出したり読み込んだり出来ます。
+任意の駒を消したり増やしたりことも出来ます。
 
 駒を英語表記に変えられます。
 
-任意の駒を消したり増やしたりことも出来ます。
+盤面をテキストデータとして書き出したり読み込んだり出来ます。
 
 
 <!--==== English description ====-->
 
-Simple Shogi board App.
+Simple Shogi board.
 
 You can move a piece freely.
 
@@ -38,11 +38,11 @@ This app based on iOS system UI design.
 
 Character adapts font size and weight by OS option.
 
-Output/Input function to/from plain text.
+You can reduce / increase pieces.
 
 English term option.
 
-You can reduce / increase pieces.
+Output/Input function to/from plain text.
 
 
 ==== Localization: All ====
