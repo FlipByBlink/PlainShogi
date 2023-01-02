58c7da1fef74f3eff37d58b086e76877205d3963
aaaaa <aaaaa>
2023-01-02 10:21:31 +0900

    UIApplication.shared.isIdleTimerDisabled = true

--- PlainShogi/Plain将棋盤App.swift
+++ PlainShogi/Plain将棋盤App.swift
@@ -11,6 +11,7 @@ struct Plain将棋盤App: App {
                 .overlay(alignment: .bottomTrailing) { 🛠メニューボタン() }
                 .sheet(isPresented: $📱.🚩メニューを表示) { 🛠AppMenu() }
                 .overlay(alignment: .bottom) { 📣ADBanner() }
+                .task { UIApplication.shared.isIdleTimerDisabled = true }
                 .environmentObject(📱)
                 .environmentObject(🛒)
         }
