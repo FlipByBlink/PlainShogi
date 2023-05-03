4cc3d4199383a165fec6c8ab52a58dcc3beb0e53
aaaaa <aaaaa>
2023-05-03 21:13:55 +0900

    WIP

--- PS Complication/PS Complication.swift
+++ PS Complication/PS Complication.swift
@@ -2,29 +2,51 @@ import WidgetKit
 import SwiftUI
 
 private struct 🄿rovider: TimelineProvider {
-    func placeholder(in context: Context) -> 🅂impleEntry {
-        🅂impleEntry()
+    func placeholder(in context: Context) -> 🄴ntry {
+        🄴ntry()
     }
-    func getSnapshot(in context: Context, completion: @escaping (🅂impleEntry) -> ()) {
-        completion(🅂impleEntry())
+    func getSnapshot(in context: Context, completion: @escaping (🄴ntry) -> ()) {
+        completion(🄴ntry())
     }
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        completion(Timeline(entries: [🅂impleEntry()], policy: .never))
+        completion(Timeline(entries: [🄴ntry()], policy: .never))
     }
 }
 
-private struct 🅂impleEntry: TimelineEntry {
+private struct 🄴ntry: TimelineEntry {
     let date: Date = .now
 }
 
-private struct 🄿SComplicationEntryView : View {
+private struct 🄴ntryView : View {
+    @Environment(\.widgetFamily) private var widgetFamily
     var body: some View {
-        ZStack {
-            AccessoryWidgetBackground()
-            Text("☖")
-                .font(.title)
+        switch self.widgetFamily {
+            case .accessoryCircular, .accessoryCorner:
+                ZStack {
+                    AccessoryWidgetBackground()
+                    GeometryReader { 📐 in
+                        Path {
+                            $0.addLines([
+                                .init(x: 50, y: 20),
+                                .init(x: 70, y: 30),
+                                .init(x: 78, y: 80),
+                                .init(x: 22, y: 80),
+                                .init(x: 30, y: 30),
+                            ])
+                            $0.closeSubpath()
+                        }
+                        .stroke(lineWidth: 6)
+                        .scaleEffect(min(📐.size.width, 📐.size.height) / 100,
+                                     anchor: .topLeading)
+                    }
+                    .padding(6)
+                }
+                .widgetAccentable()
+            case .accessoryInline:
+                Text("☖")
+            default:
+                Text("🐛")
         }
-        .widgetAccentable()
     }
 }
 
@@ -32,7 +54,7 @@ private struct 🄿SComplicationEntryView : View {
 struct 🄿SComplication: Widget {
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: "PSComplication", provider: 🄿rovider()) { _ in
-            🄿SComplicationEntryView()
+            🄴ntryView()
         }
         .configurationDisplayName("Plain将棋盤")
         .description("ショートカット")
