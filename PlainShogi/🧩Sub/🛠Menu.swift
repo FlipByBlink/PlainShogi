a8c114f0a769034d0280e3ee18c583de3c570bd8
aaaaa <aaaaa>
2023-05-10 15:36:32 +0900

    不具合フィードバック

--- PlainShogi/🧩Sub/🛠Menu.swift
+++ PlainShogi/🧩Sub/🛠Menu.swift
@@ -50,6 +50,7 @@ struct 🛠アプリメニュー: View {
                 👥SharePlay紹介リンク()
                 細かな使い方リンク()
                 テキスト書き出し読み込み紹介リンク()
+                不具合フィードバックリンク()
             }
             📣ADMenuLink()
             ℹ️AboutAppLink()
@@ -379,5 +380,64 @@ private struct テキスト変換プレビュー: View {
             }
         }
         .padding(8)
+    }
+}
+
+private struct 不具合フィードバックリンク: View {
+    var body: some View {
+        NavigationLink {
+            Self.メニュー()
+        } label: {
+            Label("不具合フィードバック", systemImage: "ladybug")
+        }
+    }
+    private struct メニュー: View {
+        @Environment(\.locale) private var locale
+        private var 日本語環境: Bool { self.locale.languageCode == "ja" }
+        private static var アドレス: String = "sear_pandora_0x@icloud.com"
+        private var ボタンURL: URL {
+            var 値 = "mailto:" + Self.アドレス
+            let タイトル: String
+            if self.日本語環境 {
+                タイトル = "☖ Plain将棋盤 不具合フィードバック 🐞"
+            } else {
+                タイトル = "☖ PlainShogiBoard bug feedback 🐞"
+            }
+            値 += "?subject="
+            値 += タイトル.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+            if self.日本語環境 {
+                値 += "&body="
+                値 += "ここに入力してください".addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+            } else {
+                値 += "&body=Input%20here"
+            }
+            return URL(string: 値)!
+        }
+        var body: some View {
+            List {
+                Section {
+                    Label("もし、このアプリでバグやクラッシュが発生した場合、以下のボタン(もしくはアドレス)からフィードバックを送るとアプリの改善に繋がります",
+                          systemImage: "ladybug")
+                    Label("特にSharePlay中に発生した不具合について報告していただけるととても助かります",
+                          systemImage: "shareplay")
+                }
+                Link(destination: self.ボタンURL) {
+                    Label("メールアプリからフィードバックを送る", systemImage: "envelope")
+                }
+                .badge(Text(Image(systemName: "arrow.up.forward.app")))
+                HStack {
+                    Label(Self.アドレス, systemImage: "link")
+                        .textSelection(.enabled)
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                    Button("コピー") {
+                        UIPasteboard.general.string = Self.アドレス
+                        💥フィードバック.軽め()
+                    }
+                    .buttonStyle(.bordered)
+                }
+            }
+            .navigationTitle("不具合フィードバック")
+        }
     }
 }
