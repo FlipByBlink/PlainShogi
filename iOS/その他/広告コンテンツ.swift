54ff8dc481e9a9853a6b196fd4bfee90d6cda08b
aaaa <a@a>
2023-10-17 18:03:02 +0900

    a

--- iOS/その他/広告コンテンツ.swift
+++ iOS/その他/広告コンテンツ.swift
@@ -3,6 +3,6 @@ import SwiftUI
 struct 広告コンテンツ: View {
     @State private var targetApp: 📣ADTargetApp = .pickUpAppWithout(.PlainShogiBoard)
     var body: some View {
-        📣ADView(self.targetApp, second: 20)
+        📣ADView(self.targetApp, second: 15)
     }
 }
