15e58fc2fb4c87909218ad7a9fa1b626e63081b8
aaaa <a@a>
2024-01-21 16:20:49 +0900

    watchOSでカスタム音声フィードバックを無効化

--- Shared/その他/フィードバック/フィードバックモデル.swift
+++ Shared/その他/フィードバック/フィードバックモデル.swift
@@ -1,20 +1,26 @@
 import Foundation
 
 class フィードバックモデル {
+#if !os(watchOS)
     private let 音声: 音声フィードバックモデル = .init()
+#endif
     func 軽め() {
         システムフィードバック.軽め()
     }
     func 強め(_ 音声あり: Bool = true) {
         システムフィードバック.強め()
+#if !os(watchOS)
         if 音声あり, self.効果音有効 { self.音声.メイン再生() }
+#endif
     }
     func 成功() {
         システムフィードバック.成功()
     }
     func エラー() {
         システムフィードバック.エラー()
+#if !os(watchOS)
         if self.効果音有効 { self.音声.ジャラジャラ再生() }
+#endif
     }
     func 警告() {
         システムフィードバック.警告()
