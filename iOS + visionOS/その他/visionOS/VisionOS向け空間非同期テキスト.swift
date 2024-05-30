ccc40e39ebc5d428830aae679baae6892c36c976
aaaa <a@a>
2024-05-30 15:16:09 +0900

    a

---
+++ iOS + visionOS/その他/visionOS/VisionOS向け空間非同期テキスト.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import GroupActivities
+
+struct VisionOS向け空間非同期テキスト: ViewModifier {
+    @EnvironmentObject var モデル: アプリモデル
+    func body(content: Content) -> some View {
+#if os(visionOS)
+        content
+            .safeAreaInset(edge: .bottom) {
+                if モデル.グループセッション?.state == .joined {
+                    Text("このメニュー表示は同期していません")
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline)
+                        .padding()
+                }
+            }
+#else
+        content
+#endif
+    }
+}
--- iOS + visionOS/サブView/シート管理.swift
+++ iOS + visionOS/サブView/シート管理.swift
@@ -24,6 +24,7 @@ struct シート管理: ViewModifier {
                             }
                         }
                         .toolbar { self.閉じるボタン() }
+                        .modifier(VisionOS向け空間非同期テキスト())
                     }
                     .environmentObject(モデル)
                     .onAppear { システムフィードバック.軽め() }
